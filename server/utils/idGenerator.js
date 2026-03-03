d6f94cbe16a9e9fbe83cbe71a334d5a06ea689c0
KS1 <shinehome888@gmail.com>
2026-03-03 02:31:59 -0800

    idGenerator.js

--- server/utils/idGenerator.js
+++ server/utils/idGenerator.js
@@ -1,7 +1,6 @@
 const generateTradeID = () => {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let result = '';
-  // Generate 8 random characters
   for (let i = 0; i < 8; i++) {
     result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
